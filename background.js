ebddeb18f0dabdaf63e9307887005f4ca5d0089c
Dhanapaul-Devop <116099658+Dhanapaul-Devop@users.noreply.github.com>
2023-10-31 13:01:38 +0530

    sdff

--- background.js
+++ background.js
@@ -1,38 +1,45 @@
 const loggedInTabs = {};
-chrome.webNavigation.onCompleted.addListener(function(details) {
-    
-    if (details.url.includes("https://nova-neo.com/StudentSupport/Logout.do") || details.url.includes("https://nova-neo.com/StudentSupport/") && !loggedInTabs[details.tabId])
-     {
-        chrome.storage.sync.get(["loginId", "loginPw", "checkboxState"], function(data) {
-            if (data.checkboxState) {
-            chrome.tabs.sendMessage(details.tabId, { action: "login", userData: data });
-            }
-        });
-    }//menu screen
-    else if (details.url.includes("https://nova-neo.com/StudentSupport/Menu.do")) {
-        chrome.tabs.sendMessage(details.tabId, { action: "menuScreenAndClick" });
-    }//student menu
-    else if (details.url.includes("https://nova-neo.com/StudentSupport/StudentMenu.do")) {
-        chrome.tabs.sendMessage(details.tabId, { action: "retrieveAndClick" });
-    }//lesson screen
-    else if (details.url.includes("https://nova-neo.com/StudentSupport/LessonMenu.do")) {
-        chrome.tabs.sendMessage(details.tabId, { action: "retrieveAndClickMenu" });
-    }//student search
-    else if (details.url.includes("https://nova-neo.com/StudentSupport/StudentSearch.do")) {
-        chrome.tabs.sendMessage(details.tabId, { action: "fillAndSubmit" });
-    }//student data popup
-    else if (details.url.includes("https://nova-neo.com/StudentSupport/StudentData.do")) {
-        chrome.tabs.sendMessage(details.tabId, { action: "studentDataClick" });
-    }//school search
-    else if (details.url.includes("https://nova-neo.com/StudentSupport/OperationSchool.do")) {
-        chrome.tabs.sendMessage(details.tabId, { action: "searchSchool" });
-    }
+
+chrome.webNavigation.onCompleted.addListener(function (details) {
+    chrome.storage.sync.get(["loginModeCheckboxState", "loginModeSelectValue"], function (data) {
+        var loginModeCheckboxState = data.loginModeCheckboxState;
+        var loginModeSelectValue = data.loginModeSelectValue;
+
+        if (loginModeCheckboxState && loginModeSelectValue == 2) {
+            handleActions("https://nova-neo.com", details);
+        } else if (loginModeCheckboxState && loginModeSelectValue == 1) {
+            handleActions("https://localhost:8080", details);
+        }
+    });
 });
 
+function handleActions(baseURL, details) {
+    if (
+        details.url.includes(`${baseURL}/StudentSupport/Logout.do`) ||
+        (details.url.includes(`${baseURL}/StudentSupport/`) && !loggedInTabs[details.tabId])
+    ) {
+        chrome.storage.sync.get(["loginId", "loginPw", "checkboxState"], function (data) {
+            if (data.checkboxState) {
+                chrome.tabs.sendMessage(details.tabId, { action: "login", userData: data });
+            }
+        });
+    } else if (details.url.includes(`${baseURL}/StudentSupport/Menu.do`)) {
+        chrome.tabs.sendMessage(details.tabId, { action: "menuScreenAndClick" });
+    } else if (details.url.includes(`${baseURL}/StudentSupport/StudentMenu.do`)) {
+        chrome.tabs.sendMessage(details.tabId, { action: "retrieveAndClick" });
+    } else if (details.url.includes(`${baseURL}/StudentSupport/LessonMenu.do`)) {
+        chrome.tabs.sendMessage(details.tabId, { action: "retrieveAndClickMenu" });
+    } else if (details.url.includes(`${baseURL}/StudentSupport/StudentSearch.do`)) {
+        chrome.tabs.sendMessage(details.tabId, { action: "fillAndSubmit" });
+    } else if (details.url.includes(`${baseURL}/StudentSupport/StudentData.do`)) {
+        chrome.tabs.sendMessage(details.tabId, { action: "studentDataClick" });
+    } else if (details.url.includes(`${baseURL}/StudentSupport/OperationSchool.do`)) {
+        chrome.tabs.sendMessage(details.tabId, { action: "searchSchool" });
+    }
+}
 
-
-//Stop once login success
-chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
+// Stop once login success
+chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
     if (request.action === "loginSuccess") {
         loggedInTabs[sender.tab.id] = true;
     }
